aed73c349c0e378f5a5dda434b58b41955b30efa
Elves Aguiar <elvesmhs@hotmail.com>
2023-01-10 17:46:30 -0300

    feat: fortuneCookie class is finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cookiePhrase:''
+    }
+
+    this.phrase = [
+      'Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+    this.openCookie = this.openCookie.bind(this);
+  }
+
+
+  openCookie(){
+    let state = this.state;
+    let radomNumber = Math.floor(Math.random()* this.phrase.length);
+    state.cookiePhrase = '"'+this.phrase[radomNumber]+'"';
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <img src={require('./assets/biscoito.png')} className="img" />
+        <Button name="open cookie" actionButton={this.openCookie}/>
+        <h3 className="cookiePhrase">{this.state.cookiePhrase}</h3>
+      </div>
+    );
+  }
 }
-
 export default App;
+
+
+class Button extends Component {
+  render() {
+    return(
+    <button onClick={this.props.actionButton}>{this.props.name}</button>);
+  }
+
+}
